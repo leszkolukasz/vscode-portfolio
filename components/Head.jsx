2d5a22d4cac6d0bd4871868ab510e4da0b74159c
Łukasz Leszko <leszko.lucas@gmail.com>
2024-10-12 22:31:01 +0200

    Add favicon

--- components/Head.jsx
+++ components/Head.jsx
@@ -20,6 +20,8 @@ const CustomHead = ({ title }) => {
       <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
       <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
       <meta name="twitter:card" content="summary_large_image" />
+
+      <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
     </Head>
   );
 };
